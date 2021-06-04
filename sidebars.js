46afad27b67aa7733fe2b3c9fc49f366e3161cf8
Gawen Monnot <79226149+GawenMonnot@users.noreply.github.com>
2021-06-04 15:23:28 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -18,7 +18,7 @@ module.exports = {
       },
     ],
     'Developers': [
-      'developer/index', 'developer/contribute', 'developer/markdown',
+      'developer/index', 'developer/contribute', 'developer/markdown', 'developer/tutorial', 'developer/errors'
     ],
   },
 };
